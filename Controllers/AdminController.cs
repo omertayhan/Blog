15636773d43c33970bb159254206dacbdcc99bc6
Ömer Tufan Ayhan <omertufan2000@hotmail.com>
2022-10-25 23:25:08 +0300

    new starting

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,12 +1,85 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<AdminController> _logger;
+        private readonly BlogContext _context;
+
+        public AdminController(ILogger<AdminController> logger, BlogContext context)
         {
-            return View();
+            _logger = logger;
+            _context = context;
         }
+
+        #region CRUD Category
+        public IActionResult Category()
+        {
+            List<Category> list = _context.Category.ToList();
+            return View(list);
+        }
+        public async Task<IActionResult> AddCategory(Category category)
+        {
+            if (category.Id == 0)
+            {
+                await _context.AddAsync(category);
+            }
+            else
+            {
+                _context.Update(category);
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Category", "Admin");
+        }
+        public async Task<ActionResult> DeleteCategory(int? id)
+        {
+            Category category = await _context.Category.FindAsync(id);
+            _context.Remove(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Category", "Admin");
+        }
+        public async Task<ActionResult> CategoryDetails(int id)
+        {
+            var category = await _context.Category.FindAsync(id);
+            return Json(category); // category transform to json
+        }
+        #endregion
+
+        #region CRUD Users
+        public IActionResult Users()
+        {
+            List<Users> list = _context.Users.ToList();
+            return View(list);
+        }
+        public async Task<IActionResult> AddUser(Users users)
+        {
+            users.IsAdmin = 0; //this gonna be change
+            if (users.Id == 0)
+            {
+                await _context.AddAsync(users);
+            }
+            else
+            {
+                _context.Update(users);
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Users", "Admin");
+
+        }
+        public async Task<ActionResult> DeleteUser(int? id)
+        {
+            Users users = await _context.Users.FindAsync(id);
+            _context.Remove(users);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Users));
+        }
+        public async Task<ActionResult> UserDetails(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            return Json(user); // category transform to json
+        }
+        #endregion
     }
 }
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,62 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+        private readonly BlogContext _context;
+
+        public LoginController(ILogger<LoginController> logger, BlogContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        #region Login and Sign in
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public IActionResult LoginControl(string email, string password)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Email == email && x.Password == password);
+            if (user == null)
+            {
+                //gonna come error msg
+                return RedirectToAction("Login","Login");
+            }
+
+            HttpContext.Session.SetInt32("id", user.Id);
+            HttpContext.Session.SetInt32("admin", user.IsAdmin);
+            return RedirectToAction("Index","Home");
+        }
+
+        public async Task<IActionResult> SignInAdd(Users users)
+        {
+            users.IsAdmin = 0; //this gonna be change
+            if (users.Id == 0)
+            {
+                await _context.AddAsync(users);
+            }
+            else
+            {
+                _context.Update(users);
+            }
+            await _context.SaveChangesAsync();
+            //gonna come success msg
+            return RedirectToAction("Index", "Home");
+        }
+        public IActionResult UserExit()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
+    }
+}
